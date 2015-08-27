2f89570f01143f3c86cf6bc28e33a503263e80ba
mogiken <mogi@hugg.co.jp>
2015-08-27 10:20:41 +0900

    登録

---
+++ mobile/src/main/java/jp/tcmobile/bijyo/AppConsts.java
@@ -0,0 +1,6 @@
+package jp.tcmobile.bijyo;
+
+public class AppConsts  {
+    //public static final String GUIDE_URL = "http://ec2-54-215-243-230.us-west-1.compute.amazonaws.com/mywatch/smp/";
+    public static final String GUIDE_URL = "http://www.tcmobile.jp/";
+}
